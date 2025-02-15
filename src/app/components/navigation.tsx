049329550ac20a792586b99e8b0af5b1eb2e0385
Jing <jing@see-mode.com>
2025-02-15 13:07:26 +1100

    basic pages setup

---
+++ src/app/components/navigation.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { useState } from "react";
+import { X, Menu } from "lucide-react";
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = ["Portfolio", "About", "Contact"].map((item, index) => {
+    return { id: index, text: item };
+  });
+
+  return (
+    <div className="bg-black flex justify-between items-center h-24 mx-auto px-4 text-white w-full ">
+      {/* Logo */}
+      <h1 className="w-full text-3xl ">NK</h1>
+
+      {/* Desktop Navigation */}
+      <ul className="hidden md:flex">
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="p-4 hover:text-[#f2d5a0] rounded-xl m-2 cursor-pointer duration-300"
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <X size={20} /> : <Menu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className="w-full text-3xl font-bold text-white m-4">NK</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="p-4 border-b rounded-xl hover:bg-[#f2d5a0] duration-300 hover:text-black cursor-pointer border-gray-600"
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
